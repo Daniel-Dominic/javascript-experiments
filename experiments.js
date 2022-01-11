1348145fe4d2ea7bd4ec9637cd2e8d15db95c937
Daniel Dominic <soulalex303@gmail.com>
2022-01-11 09:52:16 +0530

    define some functions

--- experiments.js
+++ experiments.js
@@ -1,12 +1,19 @@
-const [variable, variable1] = ["f", "g"];
-
-const timesToRepeat = "50";
-
-let [empty, empty1] = ["", ""];
-
-for(let i = 0; i < timesToRepeat; i++) {
- empty += variable
- empty1 += variable1
+function addTwo(number) {
+ return number + 2
 }
 
-console.log(empty + empty1)
+function giveBackSeven() {
+ return 5;
+}
+
+function addNum(num1, num2) {
+ return num1 + num2
+}
+
+const finalAnswer = addTwo(5)
+const finalAnswer2 = addTwo(10 + 2)
+
+console.log(addNum(5, 10))
+console.log(giveBackSeven())
+console.log(finalAnswer)
+console.log(finalAnswer2)
