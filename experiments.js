bc63b995409663bfc000f5cde9b3074bd7b5f03c
Daniel Dominic <soulalex303@gmail.com>
2022-01-11 06:02:04 +0530

    minor changes

--- experiments.js
+++ experiments.js
@@ -1,13 +1,25 @@
-function addString(string1, string2) {
- return string1 + string2
-}
-
 const timesToRepeat = 50;
 
 let empty = '';
 
+let number= "number";
+
+let number1 = number.length;
+
+function addString(string1, string2) {
+ return string1 + string2
+}
+
+let function1; 
+
+
+if (number1 == "6")
 for (let i = 0; i < timesToRepeat; i++) {
- empty += addString("f", "f")
+ empty += addString("f", "g")
+} else {
+ for(let i = 0; i < timesToRepeat; i++){
+  empty += addString("s", "s")
+ }
 }
 
 console.log(empty)
