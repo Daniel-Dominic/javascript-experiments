452477257afd241b82b60543e1845b56819a11c3
Daniel Dominic <soulalex303@gmail.com>
2022-01-17 21:24:49 +0530

    Idk mann..

--- experiments.js
+++ experiments.js
@@ -1,8 +1,16 @@
-const cat = {
- name: "Sarah",
- speak(){
-  console.log("Meow, Meow")
- }
+const [vari1, vari2] = ["F", "G"]
+
+const timesToRepeat = 25;
+
+let [empty, empty1] =  ['', ''];
+
+for (let i = 0; i < timesToRepeat; i++) {
+ empty += vari1;
+ empty1 += vari2;
 }
 
-console.log(cat.speak())
+function addString(str1, str2) {
+ return str1 + str2
+}
+
+console.log(addString(empty, empty1));
