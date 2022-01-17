27c43fb778f6e4d3467bccc3d70d4d7243c7e43b
Daniel Dominic <soulalex303@gmail.com>
2022-01-17 20:02:46 +0530

    Music suggestion for different ages

--- experiments.js
+++ experiments.js
@@ -1,11 +1,29 @@
-let person = {
+const person1 = {
  firstName: "Daniel",
  lastName: "Dominic",
- age: 13,
- countryLivingIn: "India",
- favoriteFood: "Pizza",
- pizzaWantedRightNow: true,
- numberOfPizzasWanted: 100
+ ageRange: "25-35"
+};
+
+const person2 = {
+ firstName: "Jack",
+ lastName: "Dewell",
+ ageRange: "55-65"
 }
 
-console.log(person["firstName"])
+const person3 = {
+ ageRange: "12-13"
+}
+
+function suggestMusic(person) {
+if (person.ageRange == "25-35") {
+ console.log("We guess you will like Draft Punk you crazy millennial.")
+} else if (person.ageRange == "55-65") {
+ console.log("We guess you will like Johnny Cash. He Walks the line.") 
+} else {
+ console.log("Maybe recommend Ylona Garcia? Everyone likes her right?")
+}
+}
+
+console.log(suggestMusic(person1))
+console.log(suggestMusic(person2))
+console.log(suggestMusic(person3))
