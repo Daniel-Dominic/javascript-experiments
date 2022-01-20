2c724bf1b8d4cacebd4ff5e9d1b4e15f8e959399
Daniel Dominic <soulalex303@gmail.com>
2022-01-20 12:09:21 +0530

    update

--- experiments.js
+++ experiments.js
@@ -1,25 +1,15 @@
-const me = {
- name: {
-  first: "Daniel",
-  last: "Dominic"
- },
- location: {
-  streetNumber: 500,
-  street: "Fakestreet",
-  city: "Seattle",
-  state: "WA",
-  zipCode: 5555,
-  country: "USA"
- },
- getAddress(){
-  return `${this.name.first } ${this.name.last} ${this.location.streetNumber} ${this.location.street}
-  ${this.location.city}, ${this.location.state} ${this.location.zipCode}
-  ${this.location.country}`
- }
-}
+const courses = [
+ {teacher: "Kyle Simpson", course: "Deep JS Foundations"},
+ {teacher: "Brian Holt", course: "Intro to Web Development"},
+ {teacher: "Estelle Weyl", course: "CSS In-Depth"},
+ {teacher: "Steve Kinney", course: "State Management"},
+]
 
-// setTimeout(me.name.middle.toUpperCase(), 3000) 
+courses.push({teacher: "Sean Larkin", course: "Webpack"})
 
-console.log(me.getAddress())
+var spliced = courses.splice(3, 2, {another: "course"});
 
-console.log(window.scrollY)
+
+console.log(courses)
+
+console.log(spliced)
